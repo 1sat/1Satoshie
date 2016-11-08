cfe0832e6cf6ed4312cc441082b3be52738dc915
maincryptodev <ashaleynapoleon@gmail.com>
2016-11-08 09:10:06 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1satoshie-Wallet
+<br>Version 1.1.0
+
+Welcome to the Wallet-Source of 1satoshie<br>
+we will update the README.md with build instructions soon.
+
+Also we will try to provide precompiled versions for Windows and Linux.
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define 1satoshie-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "1satoshieURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
